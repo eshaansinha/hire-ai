e570cc6144beb03faafef61c85ef9503d1ad74b6
eshaansinha <eshaan.aryan4091@gmail.com>
2025-06-21 02:46:14 +0530

    Fixed Auth Redirect (3)

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -41,7 +41,7 @@ const Landing = () => {
             <p className='text-teal-300 text-2xl font-bold italic'>
                 The only place you need to hire the best talent.
             </p>
-            <p className='bg-gray-300 text-gray-700 p-3 text-lg italic font-extrabold rounded-lg'>Get access to <ReactTyped  className="underline" typeSpeed={50} backSpeed={50} loop strings={["PeopleGPT", "Resume Parser"]}/></p>
+            <p className='bg-gray-300 text-gray-700 p-3 text-lg italic font-extrabold rounded-lg'>Get access to <ReactTyped  className="underline" typeSpeed={50} backSpeed={50} loop strings={["PeopleGPT", "Resume Parser","Smart Interview Questions"]}/></p>
             <button onClick= {() => {handlesignin()}}className='bg-teal-300 text-gray-700 shadow-teal-200 shadow-md active:translate-y-1 hover:translate-y-0.5 transition duration-300 font-extrabold text-xl rounded-3xl p-3'>Start Now</button>
         </div>
 
